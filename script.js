172784f9fd7aaef8ba254be7f4c39e52ff4bb645
oleoetken <104391010+oleoetken@users.noreply.github.com>
2026-09-02 16:20:38 +0200

    Adjust html and fix/solve js issues, add style
    Change input name and add submit button and change button to checkbox
    Fix js form to correctly submit and add preventDefault
    Add input values and reset form after submit
    Add dataset.id and deleteButton
    Add book protoype function for toggleRead
    Add css dialogForm style

--- script.js
+++ script.js
@@ -10,9 +10,16 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'read' : 'not read yet'}`;
-    };
+    /*
+        this.info = function() {
+            return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'read' : 'not read yet'}`;
+    
+        };
+    */
+}
+
+Book.prototype.toggleRead = function() {
+    this.read = !this.read;
 }
 
 /*const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
@@ -29,6 +36,7 @@ function displayBooks() {
     for (let book of myLibrary) {
         const card = document.createElement("div");
         card.classList.add("book-card");
+        card.dataset.id = book.id;
 
         const titleElement = document.createElement("p");
         titleElement.textContent = "Title: " + book.title;
@@ -42,10 +50,22 @@ function displayBooks() {
         const readElement = document.createElement("p");
         readElement.textContent = "Read: " + (book.read ? "Yes" : "No");
 
+
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.classList.add("delete-button");
+
+        const toggleReadButton = document.createElement("button");
+        toggleReadButton.textContent = "Toggle Read";
+        toggleReadButton.classList.add("toggle-read-button");
+
+
         card.appendChild(titleElement);
         card.appendChild(authorElement);
         card.appendChild(pagesElement);
         card.appendChild(readElement);
+        card.appendChild(deleteButton);
+        card.appendChild(toggleReadButton);
 
         container.appendChild(card);
 
@@ -55,7 +75,9 @@ function displayBooks() {
 
 const showButton = document.getElementById("showDialog");
 const dialogForm = document.getElementById("dialogForm");
+const bookForm = dialogForm.querySelector("form");
 const closeButton = document.getElementById("closeButton");
+const submitButton = document.getElementById("submitButton");
 
 showButton.addEventListener("click", () => {
     dialogForm.showModal();
@@ -63,6 +85,41 @@ showButton.addEventListener("click", () => {
 
 closeButton.addEventListener("click", () => {
     dialogForm.close();
+});
+
+submitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const title = document.querySelector('input[name="title"]').value;
+    const author = document.querySelector('input[name="author"]').value;
+    const pages = document.querySelector('input[name="pages"]').value;
+    const read = document.querySelector('input[name="read"]').checked;
+
+    addBooktToLibrary(title, author, pages, read);
+    displayBooks();
+
+    bookForm.reset();
+    dialogForm.close();
+
+});
+
+container.addEventListener("click", (e) => {
+    if (e.target.classList.contains("delete-button")) {
+        const card = e.target.closest(".book-card");
+        const id = card.dataset.id;
+
+        const index = myLibrary.findIndex(book => book.id === id);
+        myLibrary.splice(index, 1);
+
+        displayBooks();
+    }
+    if (e.target.classList.contains("toggle-read-button")) {
+        const card = e.target.closest(".book-card");
+        const id = card.dataset.id;
+        const book = myLibrary.find(book => book.id === id);
+        book.toggleRead();
+        displayBooks();
+    }
 })
 
 addBooktToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
